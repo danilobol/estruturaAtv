6f772098909fd8694888f13e0392ad8a4daa5d67
Danilo Brito <danilo.britoxd@hotmail.com>
2014-09-29 22:04:17 -0300

    expressão v + 3

---
+++ Lista 2/Questao 3.c
@@ -0,0 +1,5 @@
+/* 3) Suponha que os elementos do vetor v são do tipo int e cada int ocupa 8 bytes
+no seu computador. Se o endereço de v[0] é 55000, qual o valor da
+expressão v + 3?
+
+RESPOSTA: Daria erro pois v não é uma variável e sim um vetor, para somar deveria ser v[posição do vetor] + 3. */
