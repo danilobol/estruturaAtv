8be20292943e906a3bf063723c59c617e089d5c9
Danilo Brito <danilo.britoxd@hotmail.com>
2014-09-30 02:03:10 -0300

    função  string como parâmetro

---
+++ Lista 2/Questao 7.c
@@ -0,0 +1,22 @@
+/*7)Crie uma função que receba uma string como parâmetro (de tamanho
+desconhecido) e retorne uma cópia da mesma. A assinatura da função deve
+ser:
+char *strcopy(char *str);
+*/
+
+#include <stdio.h>
+
+int main(){
+	
+	char *strcopy, l;
+	void strString(char *strcopy(char *str));
+	
+	strString(strcopy);
+	
+    printf("A copia é %s", strString);
+    return 0;
+} 
+
+void strString(char *strcopy(char *str)){
+     printf("String: %s", strString); 
+}
