af9e8a1e7a8de9b54efe40d1901d2ab58e9d0e82
Danilo Brito <danilo.britoxd@hotmail.com>
2014-09-29 21:24:20 -0300

    Diferenca entre V[3] e V + 3

---
+++ Lista 2/Questao 1.c
@@ -0,0 +1,5 @@
+//Atividade 2
+//Questão 1:
+
+
+/*A diferença é que v[3] é um vetor com 3 posições e cada posição é uma memória diferentes e o v + 3 é uma operação matemática onde vai somar o valor de uma variável mais o numero 3.*/
