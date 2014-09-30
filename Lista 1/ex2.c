59a3bbc2154c17cf7dac73b834005bb80f421a33
Danilo Brito <danilo.britoxd@hotmail.com>
2014-09-30 15:59:47 -0300

    Update ex2.c

--- Lista 1/ex2.c
+++ Lista 1/ex2.c
@@ -3,13 +3,13 @@
 
 int main(void){
     
-    int trocaValores(int &x, int &z);
+    int trocaValores(int &x, int &z); // chama a função
     int x, z;
     
     scanf ("%d",&x);
     scanf ("%d",&z);
     
-    trocaValores(x, z);
+    trocaValores(x, z); //funçao recebe as variáveis
     
     printf("\n%d \n%d\n",x,z);
     system ("Pause");
@@ -18,11 +18,11 @@ int main(void){
 
 int trocaValores(int &x, int &z){
     
-    if (x > z){
-          return x, z;
+    if (x > z){ // se o 1 é maior que o 2 
+          return x, z; // retorna a mesma orde
     }
-    else{
-         int aux = x;
+    else{ // se nao for
+         int aux = x; // inverte a ordem
          x = z;
          z = aux;
          return x, z;
