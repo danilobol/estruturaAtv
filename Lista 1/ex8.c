7f290c24a251a8b5c428779dd86460130f6c9dd3
Danilo Brito <danilo.britoxd@hotmail.com>
2014-10-19 15:17:54 -0200

    Update ex8.c

--- Lista 1/ex8.c
+++ Lista 1/ex8.c
@@ -1,57 +1,52 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main (){
+int main(void)
+{
+  int a, i, max, cont;
+  
+  void rep(int *p, int a, int *max, int *cont);
+  
+  printf("Digite quantos elementos tem seu vetor?:\n");
+  scanf("%i",&a);
+  int p[a];
+  printf("Digite os elementos:\n");
+  for (i=0; i<a; i++)
+    {
+      scanf("%i", &p[i]);
+    }
     
-    void repe();
-    
-    repe();
-    
-    
-    system ("pause");
-    return 0;
+  rep(p, a, &max, &cont);
+  
+  printf("O maximo e: %i\n", max);
+  
+  printf("O maximo ocorre: %i\n", cont);
+  
+  return 0;
 }
 
-void repe(){
-     
-     int i, j, n;
-     
-     printf ("Quantos inteiros voce quer?: ");
-     scanf ("%d",&n);
-     int vetor[n];
-     int vet[n];
-     
-     for (i=0;i<n;++i){
-         vet[i] = 1;
-     }
-         
-     
-     for (i=0;i<n;++i){
-         scanf ("%i",&vetor[i]);
-     }
-     for (i=0;i<n-1;++i){
-         for (j=i+1;j<n;++j){
-             if (vetor[i] == vetor[j]){
-                          ++vet[i];
-             }
-         }
-     }
-
-     for (i=0;i<n-1;++i){
-         for (j=i+1;j<n;++j){
-             if (vet[i] < vet[j]){
-                        vet[i] = 0;
-             }
-             else if (vet[j] < vet[i]){
-                  vet[j] = 0;
-             }
-         }
-     }
-
-
-     for (i=0;i<n;++i){
-         if (vet[i] != 0){
-                    printf("O numero %d ocorreu %d vezes \n", vetor[i], vet[i]);
-         }
-     }
+void rep(int *p, int a, int *max, int *cont)
+{
+    cont = 0;
+    int i, j, aux;
+    for(i=0; i < a; i++)
+    {
+        for(j= i + 1; j < a; j++)
+        {
+            if(p[i] < p[j])
+            {
+                aux = p[i];
+                p[i] = p[j];
+                p[j] = aux;
+            }
+        }
+    }
+    *max = p[0];
+    
+    for (i=0; i < a; i++)
+    {
+        if (p[i] == *max)
+          *cont++;
+      
+    }
+    
 }
