732fe1c62afa37995b1266245bfa047baa672f92
Danilo Brito <danilo.britoxd@hotmail.com>
2014-10-19 14:08:16 -0200

    Update ex3.c

--- Lista 1/ex3.c
+++ Lista 1/ex3.c
@@ -1,47 +1,42 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(void){
-    
-    int trocaValores(int &x, int &y, int &z);
-    int x, y, z;
-    
-    scanf ("%d",&x);
-    scanf ("%d",&y);
-    scanf ("%d",&z);
-    
-    trocaValores(x, y, z);
-    
-    printf("\n%d \n%d\n%d\n", x, y, z);
-    system ("Pause");
-    return 0;
+
+int main() {
+	int a, b, c;
+	
+	int ordenar(int *a, int *b, int *c);
+	
+	int ordenar(int *a, int *b, int *c);
+	
+	scanf("%d %d %d", &a, &b, &c);
+	
+	ordenar(&a, &b, &c);
+	
+	printf ("%d %d %d",a,b,c);
+	
+	return 0;
 }
 
-int trocaValores(int &x, int &y, int &z){
-    int i[3];
-    int j, k, aux;
-    i[0] = x;
-    i[1] = y;
-    i[2] = z;
-    
-    for(j=0;j<2;++j){
-        for(k=1;k<=2;++k){
-                if (i[j] > i[k]){
-                         aux = i[j];
-                         i[j] = i[k];
-                         i[k] = aux;
-                         
-                }       
-        }
-    }
-    x = i[0];
-    y = i[1];
-    z = i[2];
-    
-    if (x == y == z){
-          return 1;
-    }
-    else{
-         return 0;
-    }
+int ordenar(int *a, int *b, int *c) {
+	if((*a == *b) & (*a == *c))
+		return 1;
+
+	if(*a > *b) {
+		*a += *b;
+		*b = *a - *b;
+		*a -= *b;
+	}
+	if(*b > *c) {
+		*b += *c;
+		*c = *b - *c;
+		*b -= *c;
+		
+		if(*a > *b) {
+			*a += *b;
+			*b = *a - *b;
+			*a -= *b;
+		}
+	}
+	
+	return 0;
 }
