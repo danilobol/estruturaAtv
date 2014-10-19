535a5bc0fd050ba19306bf1f697fc59fea864da2
Danilo Brito <danilo.britoxd@hotmail.com>
2014-10-19 16:04:25 -0200

    Update Ex10.c

--- Lista 1/Ex10.c
+++ Lista 1/Ex10.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-int x;
+
 int main(){
     
-    int SomaDobro (int a, int b);
-    int a, b;
+    int SomaDobro (int *a, int *b, int *x);
+    int a, b, x;
     scanf ("%d %d",&a,&b);
-    SomaDobro (a, b);
+    SomaDobro (&a, &b, &x);
     
     printf ("A soma do dobro de A e B eh %d \n", x);
-    system ("pause");
+
     return 0;
 }
-int SomaDobro (int a, int b){
-    a = a * 2;
-    b = b * 2;
-    x = a + b;
+int SomaDobro (int *a, int *b, int *x){
+    *a = *a * 2;
+    *b = *b * 2;
+    *x = *a + *b;
     
-    return x;
+    return *x;
 }
